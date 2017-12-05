a320ba07f69aa9eda785264ed147a04b3f6319a3
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-12-04 18:23:10 -0700

    added modal or popup confirm screen

--- client/Pages/OpenHouses.js
+++ client/Pages/OpenHouses.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button, FlatList, TouchableOpacity} from 'react-native';
+import Modal from 'react-native-modal';
 // import Orientation from 'react-native-orientation';
 
 export default class App extends Component {
   // componentDidMount() {
   //   Orientation.lockToLandscape();
   // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalVisible: false
+    }
+  }
+  _showModal = () => this.setState({ isModalVisible: true })
+  _hideModal = () => this.setState({isModalVisible: false})
   render() {
     return (
       <View style={
@@ -42,24 +51,78 @@ export default class App extends Component {
               placeholder = "Enter MLS# or Select a Listing"
               />
               <TouchableOpacity
-          style={{marginRight:40,
-            marginLeft:40,
-           marginTop:10,
-            paddingTop:10,
-            paddingBottom:10,
-            backgroundColor:'#25AAFB',
-            borderRadius:0,
-            borderWidth: 1,
-            borderColor: '#fff'}}
-          onPress={() => navigate('HomeScreen')}
-          underlayColor='#fff'
-          onPress={() => this.props.navigation.navigate('CreateOpenHouse')}
-          >
+              style={{marginRight:40,
+                marginLeft:40,
+               marginTop:10,
+                paddingTop:10,
+                paddingBottom:10,
+                backgroundColor:'#25AAFB',
+                borderRadius:0,
+                borderWidth: 1,
+                borderColor: '#fff'}}
+                onPress={this._showModal}
+                underlayColor='#fff'
+                >
           <Text style={{color:'#fff',
       textAlign:'center',
       paddingLeft : 10,
       paddingRight : 10}}>Start</Text>
- </TouchableOpacity>
+              </TouchableOpacity>
+              <Modal isVisible={this.state.isModalVisible}>
+                <View>
+                  <Text>MLS# 12345678</Text>
+                  <Text>245 North Maple Avenue, Apt101, Waco</Text>
+                  <Text>$345,000
+                  </Text>
+                </View>
+                <View>
+                <Image style={{width: 500, height: 300}}
+  source={{uri: 'https://static.pexels.com/photos/106399/pexels-photo-106399.jpeg'}} />
+                </View>
+                <View>
+                  <View>
+                  <TouchableOpacity
+                  style={{marginRight:40,
+                    marginLeft:40,
+                   marginTop:10,
+                    paddingTop:10,
+                    paddingBottom:10,
+                    backgroundColor:'#25AAFB',
+                    borderRadius:0,
+                    borderWidth: 1,
+                    borderColor: '#fff'}}
+                onPress={() => {
+                  this._hideModal();
+                  this.props.navigation.navigate('CreateOpenHouse')
+                }}
+                underlayColor='#fff'
+                >
+          <Text style={{color:'#fff',
+      textAlign:'center',
+      paddingLeft : 10,
+      paddingRight : 10}}>Confirm</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+              style={{marginRight:40,
+                marginLeft:40,
+               marginTop:10,
+                paddingTop:10,
+                paddingBottom:10,
+                backgroundColor:'white',
+                borderRadius:0,
+                borderWidth: 1,
+                borderColor: '#fff'}}
+                onPress={this._hideModal}
+                underlayColor='#fff'
+                >
+          <Text style={{color:'gray',
+      textAlign:'center',
+      paddingLeft : 10,
+      paddingRight : 10}}>Nope, Wrong Property</Text>
+              </TouchableOpacity>
+                  </View>
+                </View>
+              </Modal>
             </View>
           </View>
           <View>
