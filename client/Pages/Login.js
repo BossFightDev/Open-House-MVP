30fd30828a122eaa0e0013593ec0f44f5c729872
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-12-07 10:24:13 -0700

    styling added for login

--- client/Pages/Login.js
+++ client/Pages/Login.js
@@ -1,54 +1,169 @@
-import React, { Component } from 'react';
-import { TextInput, View, Text, Button, Image } from 'react-native';
-import { authenticateUser } from '../actions';
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import {
+  TextInput,
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import { authenticateUser } from "../actions";
+import { connect } from "react-redux";
+import { Font } from "expo";
 
 class Login extends Component {
   constructor() {
     super();
 
     this.state = {
-      username: '',
-      password: '',
-    }
+      username: "",
+      password: "",
+      fontLoaded: false
+    };
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      "Montserrat-Light": require("../Assets/fonts/Montserrat-Light.ttf"),
+      "Montserrat-Regular": require("../Assets/fonts/Montserrat-Regular.ttf"),
+      "Montserrat-Bold": require("../Assets/fonts/Montserrat-Bold.ttf")
+    });
+    this.setState({ fontLoaded: true });
   }
 
   render() {
-    return(
-      <View style={{alignItems: 'center'}}>
-        <Image source={require('../Assets/logo.png')} />
-        <Text>Username</Text>
+    return this.state.fontLoaded ? (
+      <View style={{ marginTop: 25, height: '100%', alignItems: "center", backgroundColor:"#454545"}}>
+        <Image source={require("../Assets/logo.png")} style={{width: 180, height: 65, marginTop: 30}}/>
+        <Text style={{ marginRight: '22%', marginTop: 25, fontFamily: "Montserrat-Regular", color: 'white', fontSize:8 }}>Username</Text>
         <TextInput
-          style={{borderColor: 'black', borderWidth: 1, width: '75%'}}
-          onChangeText={(username) => this.setState({ username })}
+          style={landscape.inputContainer}
+          onChangeText={username => this.setState({ username })}
           value={this.state.username}
         />
-        <Text>Password</Text>
+        <Text style={{ marginRight: '22%', fontFamily: "Montserrat-Regular", color: 'white', fontSize:8 }}>Password</Text>
         <TextInput
-          style={{borderColor: 'black', borderWidth: 1, width: '75%'}}
-          onChangeText={(password) => this.setState({ password })}
+          style={landscape.inputContainer}
+          onChangeText={password => this.setState({ password })}
           value={this.state.password}
         />
-        <Button
-          onPress={ 
-          
-            () => {
-              const validated = authenticateUser(true); // <~~ change this to true or false
-              if (validated.authenticate) this.props.navigation.navigate('OpenHouses')
-            
-            } }
-          title='Login'
-          color='black'
-        />
-        <Text>Forgot Password</Text>
+        <TouchableOpacity
+          style={landscape.buttonContainer}
+          onPress={() => {
+            const validated = authenticateUser(true); // <~~ change this to true or false
+            if (validated.authenticate)
+              this.props.navigation.navigate("OpenHouses");
+          }}
+        >
+          <Text style={landscape.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+        <Text style={{fontFamily: "Montserrat-Regular", color: 'white', fontSize:8}}>Forgot Password</Text>
         <Text> {this.props.authenticated} </Text>
       </View>
-    )
+    ) : null;
   }
 }
 
-const mapStateToProps= (state) => {
-  return {authenticated : state.authenticated}
-}
+const mapStateToProps = state => {
+  return { authenticated: state.authenticated };
+};
 
-export default connect(mapStateToProps, { authenticateUser })(Login)
+const landscape = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row"
+  },
+  logo: {
+    backgroundColor: "#454545",
+    height: "100%",
+    width: "50%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  openHouseContainer: {
+    backgroundColor: "#F1F1F1",
+    alignItems: "flex-end",
+    height: "100%"
+  },
+  inputButtonContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#DDDDDD"
+  },
+  searchBarContainer: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  searchBar: {
+    backgroundColor: "white"
+  },
+  buttonContainer: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: '28%',
+    backgroundColor: "#25AAFB",
+    borderRadius: 2
+  },
+  inputContainer: {
+      marginRight: 40,
+      marginLeft: 40,
+      marginTop: 2,
+      marginBottom: 10,
+      paddingTop: 5,
+      paddingBottom: 5,
+      width: '28%',
+      backgroundColor: "white",
+      borderRadius: 2
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontFamily: "Montserrat-Bold",
+    fontSize: 10
+  },
+  modalContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  modalIamge: {
+    height: 200,
+    width: 300,
+    alignItems: "center"
+  },
+  modalNopeButton: {
+    marginRight: 20,
+    marginLeft: 20,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "white",
+    borderRadius: 0,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  modalConfirmButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "#25AAFB",
+    borderRadius: 0,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  POHItem: {
+    backgroundColor: "white"
+  },
+  POHItemImage: {
+    width: 50,
+    height: 50
+  }
+});
+
+export default connect(mapStateToProps, { authenticateUser })(Login);
--- client/Pages/OpenHouses.js
+++ client/Pages/OpenHouses.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Modal from "react-native-modal";
+import { Font } from "expo";
 import {
   StyleSheet,
   Text,
@@ -17,7 +18,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isModalVisible: false
+      isModalVisible: false,
+      fontLoaded: false
     };
   }
   /**
@@ -28,22 +30,30 @@ export default class App extends Component {
   onLayout(e) {
     const {width, height} = Dimensions.get('window')
   }
+  async componentDidMount() {
+    await Font.loadAsync({
+      "Montserrat-Light": require("../Assets/fonts/Montserrat-Light.ttf"),
+      "Montserrat-Regular": require("../Assets/fonts/Montserrat-Regular.ttf"),
+      "Montserrat-Bold": require("../Assets/fonts/Montserrat-Bold.ttf")
+    });
+    this.setState({ fontLoaded: true });
+  }
 
   render() {
-    return (
+    return this.state.fontLoaded ? (
       <View
         style={landscape.container}
       >
         <View
           style={landscape.logo}
         >
-          <Image source={require("../Assets/logo.png")} />
+          <Image source={require("../Assets/logo.png")} style={{width: 180, height: 65}}/>
         </View>
         <View
           style={landscape.openHouseContainer}
         >
           <View style={landscape.inputButtonContainer}>
-            <Text>Create a New Open House</Text>
+            <Text style={{fontFamily: 'Montserrat-Regular', fontSize: 14, marginTop: 25, marginBottom: 10,}}>Create a New Open House</Text>
             <View style={landscape.searchBarContainer}>
               <TextInput
                 style={landscape.searchBar}
@@ -57,7 +67,7 @@ export default class App extends Component {
                 <Text
                   style={landscape.buttonText}
                 >
-                  Start
+                  START
                 </Text>
               </TouchableOpacity>
               <View style={landscape.modalContainer}>
@@ -191,12 +201,13 @@ export default class App extends Component {
           </View>
         </View>
       </View>
-    );
+      ) : null;
   }
 }
 
 const landscape = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
     flexDirection: "row"
   },
@@ -210,35 +221,45 @@ const landscape = StyleSheet.create({
   openHouseContainer: {
     backgroundColor: "#F1F1F1",
     alignItems: "flex-end",
-    height: "100%"
+    height: "100%",
+    width: "50%"
   },
   inputButtonContainer: {
+    paddingLeft: 18,
     borderBottomWidth: 1, 
     borderBottomColor: "#DDDDDD"
   },
   searchBarContainer: {
     flexDirection: "row", 
-    alignItems: "center" 
+    alignItems: "center",
+    marginBottom: 25,
+    marginRight: 20
   },
   searchBar: {
-    backgroundColor: "white"
+    paddingTop: 10,
+    paddingBottom: 5,
+    width: 200,
+    backgroundColor: "white",
+    borderRadius: 2,
+    fontFamily: 'Montserrat-Light',
+    fontSize: 10,
+    textAlign: 'center' 
   },
   buttonContainer: {
-    marginRight: 40,
-    marginLeft: 40,
-    marginTop: 10,
+    marginLeft: 5,
     paddingTop: 10,
-    paddingBottom: 10,
+    paddingBottom: 5,
     backgroundColor: "#25AAFB",
-    borderRadius: 0,
-    borderWidth: 1,
-    borderColor: "#fff"
+    width: 90,
+    borderRadius: 2,
   },
   buttonText: {
     color: "#fff",
     textAlign: "center",
     paddingLeft: 10,
-    paddingRight: 10
+    paddingRight: 10,
+    fontFamily: "Montserrat-Bold",
+    fontSize: 10
   },
   modalContainer: {
     justifyContent: "center",
