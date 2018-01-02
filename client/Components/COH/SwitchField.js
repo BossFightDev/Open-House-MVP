62efd22ea2c2a84ddf3db7ee9e6935cb1a2ac988
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2018-01-02 15:19:59 -0700

    mobile styling coh

--- client/Components/COH/SwitchField.js
+++ client/Components/COH/SwitchField.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { View, Text, Switch, StyleSheet } from "react-native";
+import { View, Text, Switch, StyleSheet, Dimensions } from "react-native";
+import { landscape, portrait } from "../../Pages/Style/create-open-style.js";
 import CustomText from "../CustomText.js";
 
+const { height, width } = Dimensions.get("window");
+const aspectRatio = height / width;
+const changeScreenOrientation = () => {
+  Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.LANDSCAPE);
+};
+let styles;
+if (aspectRatio > 1.6) {
+  styles = portrait;
+} else {
+  styles = landscape;
+  changeScreenOrientation();
+}
 const SwitchField = props => {
   return (
     <View style={props.style}>
-      <CustomText font="bold" style={{ padding: "2%" }}>
+      <CustomText font="bold" style={styles.questionText}>
         {props.question}
       </CustomText>
       <Switch
         value={props.value}
         onValueChange={props.onChange}
-        style={{ marginRight: "2%" }}
+        style={styles.switch}
       />
     </View>
   );
